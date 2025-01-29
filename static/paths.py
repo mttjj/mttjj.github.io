0b58b4daa811145d17c8b8b9779789195bc7924f
Matthew Janssen <git@matthewjj.com>
2025-01-28 20:44:46 -0600

    Update pythons scripts to prevent duplicate taxonomy terms Fixes #6

---
+++ static/paths.py
@@ -0,0 +1,48 @@
+import unicodedata
+
+def is_allowed_path_character(s, i, r):
+    if r == ' ':
+        return False
+
+    # Check for the most likely first (faster).
+    is_allowed = r.isalnum()  # Letter or digit
+    is_allowed = is_allowed or r in {'.', '/', '\\', '_', '#', '+', '~', '-', '@'}
+    is_allowed = is_allowed or unicodedata.category(r).startswith('M')  # Unicode marks
+    is_allowed = is_allowed or (r == '%' and i + 2 < len(s) and is_hex(s[i + 1]) and is_hex(s[i + 2]))
+    return is_allowed
+
+def is_hex(c):
+    return c in '0123456789abcdefABCDEF'
+
+def sanitize(s):
+    will_change = False
+    for i, r in enumerate(s):
+        if not is_allowed_path_character(s, i, r):
+            will_change = True
+            break
+
+    if not will_change:
+        # Prevent allocation when nothing changes.
+        return s
+
+    target = []
+    prepend_hyphen = False
+    was_hyphen = False
+
+    for i, r in enumerate(s):
+        is_allowed = is_allowed_path_character(s, i, r)
+
+        if is_allowed:
+            # Track explicit hyphen in input; no need to add a new hyphen if we just saw one.
+            was_hyphen = r == '-'
+
+            if prepend_hyphen:
+                # If currently have a hyphen, don't prepend an extra one.
+                if not was_hyphen:
+                    target.append('-')
+                prepend_hyphen = False
+            target.append(r)
+        elif len(target) > 0 and not was_hyphen and r.isspace():
+            prepend_hyphen = True
+
+    return ''.join(target)
