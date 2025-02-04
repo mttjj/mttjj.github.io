76c262f72966734cc71716cbd8ec852646d8a58a
Matthew Janssen <a24f2322_80b6_4016_b1a6_531132a59822@pobox.com>
2025-02-03 20:03:07 -0600

    Update paths to be able to be run from anywhere

--- .build/update_site.py
+++ .build/update_site.py
@@ -2,23 +2,24 @@ import subprocess
 from configuration import config
 
 logger = config.logger
+paths = config.paths
 
 def build_site():
     try:
         logger.info("Exporting Notes")
-        subprocess.run(["python3", "export_notes.py"], check=True)
+        subprocess.run(["python3", paths.base_dir / "export_notes.py"], check=True)
 
         logger.info("Transforming Taxonomies")
-        subprocess.run(["python3", "transform_taxonomies.py"], check=True)
+        subprocess.run(["python3", paths.base_dir / "transform_taxonomies.py"], check=True)
 
         logger.info("Transforming Diet Entries")
-        subprocess.run(["python3", "transform_diets.py"], check=True)
+        subprocess.run(["python3", paths.base_dir / "transform_diets.py"], check=True)
 
         logger.info("Moving Files")
-        subprocess.run(["python3", "move_files.py"], check=True)
+        subprocess.run(["python3", paths.base_dir / "move_files.py"], check=True)
 
         logger.info("Performing SCM Operations")
-        subprocess.run(["python3", "perform_scm_operations.py"], check=True)
+        subprocess.run(["python3", paths.base_dir / "perform_scm_operations.py"], check=True)
 
         logger.info("Site successfully updated")
 
